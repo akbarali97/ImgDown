e5cb86908543cb977c9f0df86fe4ec635669b469
akbar <akkuakbar97@gmail.com>
2020-09-12 13:36:16 +0530

    sep 12 - working code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # ImgDown-thread
+
+This is a simple script to download images of amirkhan from a given url.
+
+## Installation
+
+Step 1: If you have git CLI installed, run
+
+git clone https://github.com/akbarali97/ImgDown.git
+
+Or 
+
+download, unzip and open getImg.py from https://github.com/akbarali97/ImgDown.git
+
+
+Step 2: run 
+        python getImg.py (windows) 
+                or 
+        python3 getImg.py (Linux, Mac)
+
+Step 3: The images will be downloaded and stored in /images folder
---
+++ getImg.py
@@ -0,0 +1,18 @@
+import requests
+import concurrent.futures
+import sys
+
+imgsets = ['07','081216']
+for i in imgsets:
+    count = 1
+    while True:
+        try:
+            url = f'https://1847884116.rsc.cdn77.org/hindi/gallery/actor/aamirkhan/aamir_khan_{i}_00{count}.jpg'
+            x = requests.get(url)
+            if x.status_code == 404 or x.status_code == 500: break
+            img_filename = url.split('/')[7]
+            with open(f"./images/{img_filename}", "wb") as f:f.write(x.content)
+            count += 1
+        except Exception as e:
+            break
+
